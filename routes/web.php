04280c170cf4add98b6f53838839314c55c0ba10
nurikhsan11 <nurikhsannn2003@gmail.com>
2023-05-23 17:07:48 +0700

    modif typo

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FormController;
 use App\Http\Controllers\CekSehatController;
-use App\http\Controllers\ForminputController;
+use App\Http\Controllers\ForminputController;
 
 /*
 |--------------------------------------------------------------------------
